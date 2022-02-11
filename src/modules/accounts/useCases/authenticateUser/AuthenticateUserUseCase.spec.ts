f5ed51a56a02b8bb5c0aefe3a636b27f3765776e
Giovanny Castanharo <giovanycast@hotmail.com>
2022-02-10 21:51:46 -0300

    adicionando teste de categoria e uatenticação de usuario

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,74 @@
+import { AppError } from "../../../../errors/AppError"
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
+import { UsersRepositoryInMemory } from "../../repositories/In-memory/UserRepositoryInMemory"
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+
+let authenticateUserUseCase: AuthenticateUserUseCase
+
+let usersRepositoryImMemory: UsersRepositoryInMemory
+
+let createUserUseCase: CreateUserUseCase
+
+describe("Authenticate User", () => {
+
+    beforeEach(() => {
+        usersRepositoryImMemory = new UsersRepositoryInMemory()
+        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryImMemory)
+        createUserUseCase = new CreateUserUseCase(usersRepositoryImMemory)
+    })
+
+    it("Shoud be able to authenticate an user", async () => {
+
+        const user: ICreateUserDTO = {
+            driver_license: "3400321",
+            email: "tes@te.com",
+            name: "jamelao",
+            password: "123"
+        }
+
+        await createUserUseCase.execute(user)
+
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password
+        })
+
+        expect(result).toHaveProperty("token")
+    })
+
+    it("Shoud not be able to authenticate a non-existent user", () => {
+        expect(async () => {
+
+
+            const result = await authenticateUserUseCase.execute({
+                email: "false@email.com",
+                password: "123"
+            })
+
+        }).rejects.toBeInstanceOf(AppError)
+
+    })
+
+    it("Should not be able to authenticate with incorrect password", () => {
+
+        expect(async () => {
+            const user: ICreateUserDTO = {
+                driver_license: "3400323",
+                email: "tes@te1.com",
+                name: "jamelao1",
+                password: "123"
+            }
+
+            await createUserUseCase.execute(user)
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "Incorrect password"
+            })
+        }).rejects.toBeInstanceOf(AppError)
+    })
+
+
+
+})
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,61 @@
+import { AppError } from "../../../../errors/AppError"
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory"
+
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+let createCategoryUseCase: CreateCategoryUseCase
+
+describe("Create Category", () => {
+
+    beforeEach(() => {
+
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+    })
+
+    it("Should be able to create a new category", async () => {
+
+        const category = {
+            name: "Category test",
+            description: "Category description test"
+        }
+
+        await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description
+        })
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name)
+
+
+
+        expect(categoryCreated).toHaveProperty("id")
+        //quando cria uma cat. o repositorio cria o id,
+        //logo se id existir a cat. foi criada
+
+    })
+
+    it("Should not be able to create a new category with a name that already exists", async () => {
+
+        expect(async () => {
+            const category = {
+                name: "Category test",
+                description: "Category description test"
+            }
+
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description
+            })
+
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description
+            })
+        }).rejects.toBeInstanceOf(AppError)//para dar certo o erro tem que ser AppError
+
+    })
+
+})
