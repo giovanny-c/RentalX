1d37c96424ee4e48ec394ee7917f7de90708dd38
Giovanny Castanharo <giovanycast@hotmail.com>
2022-02-11 22:34:34 -0300

    refatorando imports dos tests

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,7 +1,7 @@
-import { AppError } from "../../../../errors/AppError"
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
-import { UsersRepositoryInMemory } from "../../repositories/In-memory/UserRepositoryInMemory"
-import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { AppError } from "@errors/AppError"
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO"
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/In-memory/UserRepositoryInMemory"
+import { CreateUserUseCase } from "@modules/accounts/useCases/createUser/CreateUserUseCase"
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 
 let authenticateUserUseCase: AuthenticateUserUseCase
