2adeb58527c7ee327057db1b54a54af32af1c6b0
Giovanny Castanharo <giovanycast@hotmail.com>
2022-03-30 15:21:35 -0300

    database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,5 +1,37 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm"
 
+
+export default async (): Promise<Connection> => { //host = nome do serviço do bd no docker-compose.yml
+    const defaultOptions = await getConnectionOptions()
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+
+            database: process.env.NODE_ENV === "test" ? "rentalx_test" : defaultOptions.database // se NODE_ENV for === test usa o banco de teste, se nao usa o padrao
+
+        })
+    )
+}
+
+/*
+    REMOVER QUANDO COLOCAR EM PRODUÇÃO
+    host = "database_ignite"
+*/
+
+/*
+    REMOVER QUANDO COLOCAR EM PRODUÇÃO
+    host: process.env.NODE_ENV === "test" ? "localhost" : host,
+*/
+
+//ver no package.json
+//"test": "set NODE_ENV=test & jest" ou (nao funcionou)
+//"test": "NODE_ENV=test & jest" ou (funcionou)
+//"cross-env NODE_ENV=test&&jest --runInBand --detectOpenHandles"
+//yarn add cross-env -D
+
+
+//jeito antigo
+
 // interface IOptions {
 //     host: string
 // }
@@ -13,20 +45,4 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm"
 //     })
 // })
 
-//                           database_ignite ?
-export default async (host = "database_ignite"): Promise<Connection> => { //precisa ser desse jeito para passa a conexao pro seed/admin
-    const defaultOptions = await getConnectionOptions()
-
-    return createConnection(
-        Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
-            database: process.env.NODE_ENV === "test" ? "rentalx_test" : defaultOptions.database // se NODE_ENV for === test usa o banco de teste, se nao usa o padrao
-            //ver no package.json
-            //"test": "set NODE_ENV=test & jest" ou (nao funcionou)
-            //"test": "NODE_ENV=test & jest" ou (funcionou)
-            //"cross-env NODE_ENV=test&&jest --runInBand --detectOpenHandles"
-            //yarn add cross-env -D
-
-        })
-    )
-}
+//  
