14eca756ca44d6b5d8378d001b02fe110f0bb8f2
Giovanny Castanharo <giovanycast@hotmail.com>
2022-04-19 14:40:05 -0300

    adding sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,5 @@
 //import "reflect-metadata" //yarn add reflect-metadata (se pedir) para tsyringe
-const express = require("express")
+import * as express from "express"
 
 import { NextFunction, Request, Response } from "express"
 
@@ -8,6 +8,10 @@ import "express-async-errors"
 import * as swaggerUi from "swagger-ui-express"
 
 import * as cors from "cors"
+
+import * as Sentry from "@sentry/node";
+
+import * as Tracing from "@sentry/tracing";
 
 import createConnection from "@shared/infra/typeorm" //importa o index(arquivo que cria a conexao com o banco de dados)
 
@@ -35,6 +39,26 @@ const app = express()
 
 app.use(rateLimiter)
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json())
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))//configurando o swagger
@@ -49,6 +73,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 app.use(cors())
 
 app.use(router)
+
+app.use(Sentry.Handlers.errorHandler())
 
 //middleware de error, sempre passar o erro primeiro nesse tipo de middleware
 //vai usar esse middleware em todas as rotas
